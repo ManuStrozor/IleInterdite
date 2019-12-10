3e7ac7b90436ca8ae67e719d52a85260af609645
TURBET-DELOF Emmanuel <turbetde@hostname.iut2.upmf-grenoble.fr>
2019-12-10 13:13:44 +0100

    classes propres prete pour le codage intensif !

--- src/Aventurier.java
+++ src/Aventurier.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.ileinterdite2019;
-
 /**
  *
  * @author estevmat
  */
 public class Aventurier {
-    private String nomJoueur;
-    private String test;
-    private String test2;
-    private int manu;
-    private String hamza = "le plus beau du monde";
-    private int Wiss;
+
 }
