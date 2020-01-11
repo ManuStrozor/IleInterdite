c3161535bc78e081e934696ed9d2fb1c333005c0
HamzaMakri <hamza.makri@outlook.com>
2020-01-11 19:16:25 +0100

    update Aventurier

--- src/aventuriers/Aventurier.java
+++ src/aventuriers/Aventurier.java
@@ -8,6 +8,8 @@ public abstract class Aventurier {
     private Couleur couleurPion;
     private int nbCartes;
     private String actionsRestantes;
+    private game.Tuile tuile;
+    private game.Carte[] inventaire;
 
     public int getNombreCarte(){
 
