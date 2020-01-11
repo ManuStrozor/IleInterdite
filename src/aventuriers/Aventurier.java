db81de8c1ab355f64f4564cc3c88615fbdb6d479
HamzaMakri <hamza.makri@outlook.com>
2020-01-11 18:53:29 +0100

    Mise en relation avec le diagramme de classe

--- src/aventuriers/Aventurier.java
+++ src/aventuriers/Aventurier.java
@@ -5,5 +5,20 @@ package aventuriers;
  * @author estevmat
  */
 public class Aventurier {
+    private Couleur couleurPion;
+    private int nbCartes;
+    private String actionsRestantes;
+
+    public int getNombreCarte(){
+
+    }
+
+    public void ajouterCarte(Carte carte){
+
+    }
+
+    public void defausseCarte(){
+
+    }
 
 }
